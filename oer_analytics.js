308de8089ec8245e0378354891abf3d13c3f77ca
jczetta <jczetta@gmail.com>
2013-04-25 17:19:18 -0400

    working display for views, dls, yt views
    database error and db_update TODO for first release

---
+++ oer_analytics.js
@@ -0,0 +1,14 @@
+
+
+//alert("hello, testing js!");
+
+//alert("does this work now?");
+
+function hide(divID) {
+	var item = document.getElementById(divID);
+	if (item) {
+		item.visibility = 'hidden';
+	}
+}
+
+
