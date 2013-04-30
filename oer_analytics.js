ec91abe9dd0f060a05441efbee7664bc02fd462e
jczetta <jczetta@gmail.com>
2013-04-29 20:42:21 -0400

    working on js hideshow, some module cleanups, there's extra code in here, etc

--- oer_analytics.js
+++ oer_analytics.js
@@ -4,11 +4,49 @@
 
 //alert("does this work now?");
 
-function hide(divID) {
-	var item = document.getElementById(divID);
-	if (item) {
-		item.visibility = 'hidden';
-	}
+if (Drupal.jsEnabled) {
+	
+	document.documentElement.className = 'js';
+	document.cookie = 'has_js=1; path=/';
+
+	$(document).ready(Drupal.attachBehaviors);
 }
+
+Drupal.behaviors.oer_analyticsBehavior = function () {
+	var showing = true;
+
+	$("#oer-analytics-button").hover(function (){
+		$("#oer-analytics-button").fadeOut();
+		$("#overall-show").fadeIn();
+		showing = false;
+	});
+
+	if (!showing) {
+		$('#overall-show').hover(function (){
+			$('#oer-analytics-button').fadeIn();
+			$('#overall-show').fadeOut();
+		});
+	}
+
+	// $("#overall-show").hover(function(){
+	// 	$('#oer-analytics-button').show();
+	// 	//$('#overall-show').hide();
+	// });
+};
+
+// 	var div_clicked = false;
+
+// 	$('#oer-analytics-button').click(function () {
+// 		div_clicked = true;
+// 	});
+
+// 	$(document).click(function () {
+// 		if (!div_clicked) {
+// 			$('#oer-analytics-button').hide);
+// 		}
+// 		div_clicked = false;
+// 	});
+
+// }
 
 
