7f6ee1c4d145232b54152e17991cf112a6a5d76d
jczetta <jczetta@gmail.com>
2013-05-16 09:45:40 -0400

    fix page reload; a -> trnsparent button

--- oer_analytics.js
+++ oer_analytics.js
@@ -5,19 +5,25 @@ if (Drupal.jsEnabled) {
 	document.documentElement.className = 'js';
 	document.cookie = 'has_js=1; path=/';
 
-	$(document).ready(Drupal.attachBehaviors);
+	//$(document).ready(Drupal.attachBehaviors);
+	Drupal.attachBehaviors;
+    console.log("Page load?");
 }
+
 
 Drupal.behaviors.oer_analyticsBehavior = function () {
 	$("#oer-analytics-button").hide();
 	$("#oer-analytics-buttona").click(function () {
 		$("#oer-analytics-button").fadeIn();
-		$("#oer-analytics-buttona").css('display', 'none');
+		//$("oer-analytics-button").css('display', 'block');
+		//$("#oer-analytics-buttona").css('display', 'none');
+		$("#oer-analytics-buttona").hide();
 	});
 
 	$("#hiddenq").click(function () {
+		$("#oer-analytics-button").hide(); //css('display','none');
 		$("#oer-analytics-buttona").fadeIn();
-		$("#oer-analytics-button").css('display','none');
+		
 	});	
 
 };
