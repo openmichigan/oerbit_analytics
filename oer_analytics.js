5737c2c7ab2d1d8a9fe239c3b8ec4f4b9d372c16
jczetta <jczetta@gmail.com>
2013-05-17 11:09:05 -0400

    original #s button display:none
    (hopefully) no more stats display loading before hiding on switching pages

--- oer_analytics.js
+++ oer_analytics.js
@@ -12,7 +12,7 @@ if (Drupal.jsEnabled) {
 
 
 Drupal.behaviors.oer_analyticsBehavior = function () {
-	$("#oer-analytics-button").hide();
+	//$("#oer-analytics-button").hide();
 	$("#oer-analytics-buttona").click(function () {
 		$("#oer-analytics-button").fadeIn();
 		//$("oer-analytics-button").css('display', 'block');
